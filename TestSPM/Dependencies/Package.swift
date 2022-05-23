1143c41240b99f3268da4519f5cbc55a17503005
Prudhvi <prudhvikrishna.moturi@criticalriver.com>
2022-05-23 16:09:15 +0530

    Moved package file to dependencies

--- TestSPM/Dependencies/Package.swift
+++ TestSPM/Dependencies/Package.swift
@@ -5,16 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "Dependencies",
+    platforms: [.macOS(.v10_12),
+                .iOS(.v10),
+                .tvOS(.v10),
+                .watchOS(.v3)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Dependencies",
             targets: ["Dependencies"])
     ],
-    platforms: [.macOS(.v10_12),
-                .iOS(.v10),
-                .tvOS(.v10),
-                .watchOS(.v3)],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/prudhvikrishnamoturi/RestManager", from: "1.0.0"),
@@ -24,14 +24,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Dependencies",
+            dependencies: ["RestManager"],
             path: "Source",
-            exclude: ["Info.plist"],
-            dependencies: ["RestManager"]),
+            exclude: ["Info.plist"]
+            ),
         .testTarget(
             name: "DependenciesTests",
-            path: "Tests",
-            exclude: ["Info.plist", "Test Plans"],
-            resources: [.process("Resources")],
             dependencies: ["Dependencies"])
     ]
     
